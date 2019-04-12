fe75657622cbbc2d1b40dd57e22ff2a7c19da3c8
Vladislav Stashevskiy <fnsdevelopment@gmail.com>
2019-04-12 22:27:39 +0300

    Test

--- django/core/urls.py
+++ django/core/urls.py
@@ -22,10 +22,3 @@ urlpatterns = [
     path('announcement/<str:slug>/', AnnouncementDetailView.as_view(), name='announcement'),
     
 ]
-
-
-categories = Category.objects.all()
-
-for cat in categories:
-    form = FormGenerator.generate(cat.slug, cat.specifications)
-    GENERATED_FORMS[cat.slug] = form
