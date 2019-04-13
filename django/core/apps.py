a94805d811df2856fc5c19896757fac4f8a9ac68
Vladislav Stashevskiy <fnsdevelopment@gmail.com>
2019-04-13 19:46:51 +0300

    Form generation fix #4

--- django/core/apps.py
+++ django/core/apps.py
@@ -1,11 +1,11 @@
 from django.apps import AppConfig
-from core.models import Category
-from core.forms import FormGenerator, GENERATED_FORMS
 
 
 class CoreConfig(AppConfig):
     name = 'core'
     def ready(self):
+        from core.models import Category
+        from core.forms import FormGenerator, GENERATED_FORMS
         categories = Category.objects.all()
         for cat in categories:
             form = FormGenerator.generate(cat.slug, cat.specifications)
