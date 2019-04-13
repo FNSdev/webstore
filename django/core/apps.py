5a2f0999017b679a0bfb235459524f79fdb5d56f
Vladislav Stashevskiy <fnsdevelopment@gmail.com>
2019-04-13 20:28:43 +0300

    Form generation fix #7

--- django/core/apps.py
+++ django/core/apps.py
@@ -1,9 +1,12 @@
 from django.apps import AppConfig
-
+import sys
 
 class CoreConfig(AppConfig):
     name = 'core'
     def ready(self):
+        if 'collectstatic' in sys.argv:
+            return True
+        
         from core.models import Category
         from core.forms import FormGenerator, GENERATED_FORMS
         categories = Category.objects.all()
