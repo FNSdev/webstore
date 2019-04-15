50c423e380c9b6c3da147a7e0b1854b2d3f034d2
Vladislav Stashevskiy <fnsdevelopment@gmail.com>
2019-04-15 19:20:35 +0300

    Fixind DB #4

--- bash_scripts/collect_static.sh
+++ bash_scripts/collect_static.sh
@@ -8,7 +8,7 @@ export DJANGO_CACHE_TIMEOUT=100
 export DJANGO_SECRET_KEY=FAKE_KEY
 export DJANGO_ALLOWED_HOSTS=127.0.0.1
 # settings module is important !
-export DJANGO_SETTINGS_MODULE=config.production_settings
+export DJANGO_SETTINGS_MODULE=config.settings
 
 cd $root/django/
 
